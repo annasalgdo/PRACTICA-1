8b01c39b87650a4c9404298590024857233b6d04
dawnavarro <smxmnavarro>
2017-09-08 09:23:16 +0200

    Fet des de dawnavarro

--- proyecto_anna.php
+++ proyecto_anna.php
@@ -1,1 +1,1 @@
-<?php echo "Aquest fitxer esta fet utilitzant la comanda git config  --global user.name dawnavarro" ?>
+<?php echo "Fitxer fet utilitzant la comanda git config --global user.name dawnavarro" ?>
