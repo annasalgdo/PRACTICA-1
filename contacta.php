22372375b2b417e1e1735e5908e87db30e70c5d5
annasalgdo <31473745+annasalgdo@users.noreply.github.com>
2017-09-08 12:27:57 +0200

    Create contacta.php

---
+++ contacta.php
@@ -0,0 +1,32 @@
+<div class="divider-new">
+    <h2 class="h2-responsive wow fadeIn">Contacta amb nosaltres</h2>
+</div>
+<!--Section: Contact-->
+<section id="contact">
+    <div class="row">
+        <!--First column-->
+        <div class="col-md-8">
+            <div id="map-container" class="z-depth-1 wow fadeIn" style="height: 300px"></div>
+        </div>
+        <!--/First column-->
+
+        <!--Second column-->
+        <div class="col-md-4">
+            <ul class="text-center">
+                <li class="wow fadeIn" data-wow-delay="0.2s"><i class="fa fa-map-marker teal-text"></i>
+                    <p>New York, NY 10012, USA</p>
+                </li>
+
+                <li class="wow fadeIn" data-wow-delay="0.3s"><i class="fa fa-phone teal-text"></i>
+                    <p>+ 01 234 567 89</p>
+                </li>
+
+                <li class="wow fadeIn" data-wow-delay="0.4s"><i class="fa fa-envelope teal-text"></i>
+                    <p>contacta@mdbootstrap.com</p>
+                </li>
+            </ul>
+        </div>
+        <!--/Second column-->
+    </div>
+</section>
+<!--Section: Contact-->
